7a9441c951db3ae41bf466f5f9d0085812d900d1
CHANDRAKANT <CHANDRAKANT@LAPTOP-BH0J0IF2>
2024-06-06 13:51:29 +0530

    Second repository

--- TestGitrepositeryFebBatch/src/demoGitR/TestGitR.java
+++ TestGitrepositeryFebBatch/src/demoGitR/TestGitR.java
@@ -4,6 +4,8 @@ public class TestGitR {
 
 	public static void main(String[] args) {
 		System.out.println("I AM READY TO APLOAD GIT REPO");
+		
+		System.out.println("I AM APLOADED GITHUB REPO");
 	}
 
 }
